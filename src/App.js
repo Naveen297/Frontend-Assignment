d217e380b37f9be69891b4b2788d7793ce742310
Naveen297 <naveenmalhotra148@gmail.com>
2023-04-21 22:32:42 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-// src/App.js
+// // src/App.js
+// import React, { useState } from "react";
+// import JsonEditor from "./JsonEditor";
+// import FormRenderer from "./FormRenderer";
+// import "./App.css";
+
+// function App() {
+//   const [schema, setSchema] = useState({});
+
+//   const handleJsonChange = (newSchema) => {
+//     setSchema(newSchema);
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="container mx-auto">
+//         <h1 className="text-4xl font-bold my-5 flex justify-center items-center">
+//           Dynamic UI Form Generator
+//         </h1>
+//         <h2 className="text-2xl font-semibold my-5 flex justify-center items-center">
+//           Instantly Render Forms from UI Schema
+//         </h2>
+//         {/* : Instantly Render Forms from UI Schema */}
+//         <div className="grid grid-cols-2 gap-5">
+//           <div className="border p-5">
+//             <h2 className="text-xl font-semibold mb-5 flex justify-center items-center">
+//               UI Schema
+//             </h2>
+//             <JsonEditor onChange={handleJsonChange} />
+//           </div>
+//           <div className="border p-5">
+//             <h2 className="text-xl font-semibold mb-5 flex justify-center items-center">
+//               Rendered Form
+//             </h2>
+//             <FormRenderer schema={schema} />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import JsonEditor from "./JsonEditor";
 import FormRenderer from "./FormRenderer";
 import "./App.css";
+// import logo from "./logo.png"; // Import the logo/
+// Add these imports to the top of your file
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Add this line to register the icon sets
+library.add(fas);
 
 function App() {
   const [schema, setSchema] = useState({});
@@ -14,13 +66,23 @@ function App() {
   return (
     <div className="App">
       <div className="container mx-auto">
-        <h1 className="text-4xl font-bold my-5 flex justify-center items-center">
+        {/* Add this div for the logo */}
+        <div className="absolute top-0 left-0 m-5">
+          <img
+            src="https://i.postimg.cc/KjsFjcyv/Screenshot-2023-04-21-at-10-16-51-PM-removebg-preview.png"
+            alt="Logo"
+            style={{ width: "200px" }}
+          />
+        </div>
+        {/* End logo */}
+        <h1 className="text-4xl font-bold my-5 flex justify-center items-center gradient-text">
+          <FontAwesomeIcon icon={["fas", "star"]} className="mr-2" />
           Dynamic UI Form Generator
         </h1>
-        <h2 className="text-2xl font-semibold my-5 flex justify-center items-center">
+        <h2 className="text-2xl font-semibold my-5 flex justify-center items-center gradient-text">
           Instantly Render Forms from UI Schema
         </h2>
-        {/* : Instantly Render Forms from UI Schema */}
+
         <div className="grid grid-cols-2 gap-5">
           <div className="border p-5">
             <h2 className="text-xl font-semibold mb-5 flex justify-center items-center">
@@ -41,39 +103,3 @@ function App() {
 }
 
 export default App;
-// import React, { useState } from "react";
-// import JsonEditor from "./JsonEditor";
-// import FormRenderer from "./FormRenderer";
-// import "./App.css";
-
-// function App() {
-//   const [schema, setSchema] = useState({});
-
-//   const handleJsonChange = (content) => {
-//     if (content.jsObject) {
-//       setSchema(content.jsObject);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <div className="container mx-auto">
-//         <h1 className="text-4xl font-bold my-5 text-center">
-//           UI Schema Form Renderer
-//         </h1>
-//         <div className="grid grid-cols-2 gap-5">
-//           <div className="border p-5">
-//             <h2 className="text-2xl font-semibold mb-5">UI Schema</h2>
-//             <JsonEditor onChange={handleJsonChange} />
-//           </div>
-//           <div className="border p-5">
-//             <h2 className="text-2xl font-semibold mb-5">Rendered Form</h2>
-//             <FormRenderer schema={schema} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/FormRenderer.js
+++ src/FormRenderer.js
@@ -1,56 +1,70 @@
+// // src/FormRenderer.js
+// import React from "react";
+// import { withTheme } from "@rjsf/core";
+// import { Theme as DefaultTheme } from "@rjsf/bootstrap-4";
+// import Ajv from "ajv";
+// import "./FormRenderer.css";
+
+// const ajv = new Ajv({ allErrors: true, verbose: true });
+
+// const Form = withTheme(DefaultTheme);
+
+// const FormRenderer = ({ schema }) => {
+//   return (
+//     <div style={{ height: "100vh", overflowY: "scroll" }}>
+//       <div className="form-container">
+//         <Form schema={schema} validator={ajv} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FormRenderer;
 // src/FormRenderer.js
-import React from "react";
+import React, { useState } from "react";
 import { withTheme } from "@rjsf/core";
 import { Theme as DefaultTheme } from "@rjsf/bootstrap-4";
 import Ajv from "ajv";
+import "./FormRenderer.css";
 
 const ajv = new Ajv({ allErrors: true, verbose: true });
 
 const Form = withTheme(DefaultTheme);
 
 const FormRenderer = ({ schema }) => {
+  const [formData, setFormData] = useState({});
+
+  const handleOnChange = ({ formData }) => {
+    setFormData(formData);
+  };
+
+  // Sort the fields according to the 'sort' value
+  if (schema && schema.properties) {
+    schema.properties = Object.fromEntries(
+      Object.entries(schema.properties).sort((a, b) => {
+        const aSort = a[1].sort || Number.MAX_VALUE;
+        const bSort = b[1].sort || Number.MAX_VALUE;
+        return aSort - bSort;
+      })
+    );
+  }
+
+  // Implement other rules and descriptions here
+  // Note that the current UI code doesn't support all the features described
+  // You may need to create custom components or extend the @rjsf/bootstrap-4 theme to support them
+
   return (
     <div style={{ height: "100vh", overflowY: "scroll" }}>
-      <Form schema={schema} validator={ajv} />
+      <div className="form-container">
+        <Form
+          schema={schema}
+          formData={formData}
+          onChange={handleOnChange}
+          validator={ajv}
+        />
+      </div>
     </div>
   );
 };
 
 export default FormRenderer;
-// import React from "react";
-// import { withTheme } from "@rjsf/core";
-// import { Theme as DefaultTheme } from "@rjsf/bootstrap-4";
-// import Ajv from "ajv";
-
-// const ajv = new Ajv({ allErrors: true, verbose: true });
-// const Form = withTheme({
-//   ...DefaultTheme,
-//   styles: {
-//     ...DefaultTheme.styles,
-//     row: "mb-4",
-//     controlLabel: "font-bold",
-//     selectWidget: "border rounded p-2",
-//     inputWidget: "border rounded p-2",
-//   },
-// });
-
-// const FormRenderer = ({ schema }) => {
-//   return (
-//     <Form
-//       schema={schema}
-//       validate={ajv}
-//       formData={{}}
-//       className="my-form rounded-xl border p-4"
-//       FieldTemplate={({ id, label, children, displayLabel }) => {
-//         return (
-//           <div className="mb-4">
-//             {displayLabel && <label htmlFor={id}>{label}</label>}
-//             {children}
-//           </div>
-//         );
-//       }}
-//     />
-//   );
-// };
-
-// export default FormRenderer;
